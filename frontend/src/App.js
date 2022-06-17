401625ed6173c21e711eb7beddc082fdc7f6c009
chepa-bhargavi-sai <bhargavi.chepa@gmail.com>
2022-06-17 19:06:49 +0530

    added doctor components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,8 +6,11 @@ import Header from './Components/Common/Header';
 import Login from './Components/Login';
 import InvalidPage from './Components/Common/InvalidPage';
 import Dashboard from './Components/Dashboard';
+import Dashboard2 from './Components/Dashboard2';
+import Consultation from './Components/Consultation';
 import PatientProfile from './Components/PatientProfile';
 import PatientRegistration from './Components/PatientRegistration';
+import DoctorProfile from './Components/DoctorProfile';
 import TakeTest from './Components/TakeTest';
 import Home from './Components/Home';
 
@@ -20,8 +23,11 @@ function App() {
         <Routes>
           {isLoggedIn ? <>
             <Route path='/dashboard' element={<Dashboard />} />
+            <Route path='/dashboard2' element={<Dashboard2 />} />
+            <Route path='/Consultation' element={<Consultation />} />
             <Route path='/patientProfile/:healthId' element={<PatientProfile />} />
             <Route path='/registerPatient' element={<PatientRegistration />} />
+            <Route path='/doctorProfile/:doctorId' element={<DoctorProfile />} />        
             <Route path='/takeTest/:healthId' element={<TakeTest />} />
           </> :
             <>
